fd04f7dca5a80418dcacbe6fd92cfcd83c335083
Nek-12 <vaizin.nikita@gmail.com>
2022-04-28 21:28:53 +0300

    update colors

--- core/src/main/kotlin/com/nek12/beautylab/core/model/net/Color.kt
+++ core/src/main/kotlin/com/nek12/beautylab/core/model/net/Color.kt
@@ -5,7 +5,26 @@ enum class Color(val value: Int) {
 
     RED(0xFF00),
     GREEN(0x00FF00),
-    BLUE(0x0000FF);
+    BLUE(0x0000FF),
+    WHITE(0xFFFFFF),
+    BLACK(0x000000),
+    YELLOW(0xFFFF00),
+    ORANGE(0xFFA500),
+    PURPLE(0x800080),
+    PINK(0xFFC0CB),
+    BROWN(0xA52A2A),
+    GREY(0x808080),
+    SILVER(0xC0C0C0),
+    GOLD(0xFFD700),
+    LIME(0x00FF00),
+    CYAN(0x00FFFF),
+    MAGENTA(0xFF00FF),
+    TURQUOISE(0x40E0D0),
+    OLIVE(0x808000),
+    MAROON(0x800000),
+    NAVY(0x000080),
+
+    OTHER(0x000000);
 
     companion object {
 
